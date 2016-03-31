b8eb6abbf8f1b621f76f4e0e3761caf66e7944b1
unknown <cilagan@DIS-CILAGAN-01.ad.nsf.gov>
2016-03-31 14:58:55 -0400

    Initial Commit

---
+++ PSM_Services/solicatationService/src/main/java/gov/nsf/psm/SolicitationServiceApplication.java
@@ -0,0 +1,12 @@
+package gov.nsf.psm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitationServiceApplication.class, args);
+	}
+}
